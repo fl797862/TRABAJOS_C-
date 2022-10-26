65d9b63e0b30d35c5e3483e23572d626e992a87b
fl797862 <116688636+fl797862@users.noreply.github.com>
2022-10-25 19:38:51 -0500

    Add files via upload

---
+++ API_Ejemplo/API_Ejemplo/Controllers/UsuarioController.cs
@@ -0,0 +1,86 @@
+﻿using MongoDB.Driver;
+using API_Ejemplo.Entidades;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_Ejemplo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        MongoClient cliente;
+        IMongoDatabase db;
+        public UsuarioController()
+        {
+            cliente = new MongoClient("mongodb+srv://" +
+                "fl797862:0lB1fhLUDdbwcppf@lecturas0.ofekenk.mongodb.net/" +
+                "?retryWrites=true&w=majority");
+            db = cliente.GetDatabase("Lecturas0");
+        }
+
+        private IMongoCollection<Usuario> coleccion() =>
+            db.GetCollection<Usuario>("API_Ejemplo");
+
+        [HttpGet]
+        public IEnumerable<Usuario> Get()
+        {
+            return coleccion().AsQueryable();
+        }
+
+        [HttpGet("{id}")]
+        public Usuario Get(string id)
+        {
+
+            return coleccion().Find(d => d.Id == id).SingleOrDefault();
+        }
+
+
+        [HttpPost]
+        public Usuario Post([FromBody] Usuario value)
+        {
+            try
+            {
+
+                value.Id = Guid.NewGuid().ToString();
+                coleccion().InsertOne(value);
+                return value;
+            }
+            catch (Exception)
+            {
+                return value;
+            }
+        }
+
+
+        [HttpPut("{id}")]
+        public Usuario Put(string id, [FromBody] Usuario value)
+        {
+            try
+            {
+                coleccion().ReplaceOne(d => d.Id == id, value);
+                return value;
+            }
+            catch (Exception)
+            {
+                return value;
+            }
+
+        }
+
+
+        [HttpDelete("{id}")]
+        public bool Delete(string id)
+        {
+            try
+            {
+                coleccion().DeleteOne(d => d.Id == id);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+        }
+    }
+}
---
+++ API_Ejemplo/API_Ejemplo/Entidades/Usuario.cs
@@ -0,0 +1,11 @@
+﻿namespace API_Ejemplo.Entidades
+{
+    public class Usuario
+    {
+        public string Id { get; set; }
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public string Correo { get; set; }
+        public DateTime FechaRegistro{ get; set; }
+    }
+}
